dbfc0b447ee11bea1eb9a4e2537ee395dd91dd64
Michelle Olivieri <michelleo72@gmail.com>
2018-04-16 23:37:12 +0000

    scraper edits

--- museum_finder/lib/museum_finder.rb
+++ museum_finder/lib/museum_finder.rb
@@ -1,7 +1,3 @@
+
 require_relative './museum_finder/version'
-
 require_relative './museum_finder/cli'
-
-#module MuseumFinder
-  # Your code goes here...
-#end
--- museum_finder/lib/museum_finder/scraper.rb
+++ museum_finder/lib/museum_finder/scraper.rb
@@ -1,7 +1,7 @@
 class MuseumFinder::Scraper
 
-  def self.scrape_museum_listings_page
-
+def self.scrape_museums_landing_page
+    doc = Nokogiri::HTML(open("https://www.si.edu/museums"))
   end
 
   def self.scrape_museum_info_page
