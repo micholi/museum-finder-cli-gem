418a3c536a887fbeda384685a1cf707ce208cefc
Michelle Olivieri <michelleo72@gmail.com>
2018-04-16 19:44:43 +0000

    adding placeholder code to cli file for testing purposes

--- museum_finder/lib/museum_finder/cli.rb
+++ museum_finder/lib/museum_finder/cli.rb
@@ -1,9 +1,17 @@
 class MuseumFinder::CLI
 
   def call
-    # initial code for testing purposes
-    puts "Welcome to the Smithsonian Institution!"
-  end
+      # initial code for testing purposes
+      puts "WELCOME TO THE SMITHSONIAN INSTITUTION!"
+      puts "\n"
+      puts "The Smithsonian Institution is the world's largest museum, education, and research complex."
+      puts "The Smithsonian offers 17 museums, galleries, and a zoo in the Greater Washington, DC area and 2 additional museums in New York City."
+      puts "\n"
+      menu
+    end
 
+    def menu
+      #put code here
+    end
 
 end
