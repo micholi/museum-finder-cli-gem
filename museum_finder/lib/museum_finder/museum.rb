8e115fb26670039f820b656d1e2aaddd7bec22df
Michelle Olivieri <michelleo72@gmail.com>
2018-04-17 20:54:47 +0000

    museum edits

--- museum_finder/lib/museum_finder/museum.rb
+++ museum_finder/lib/museum_finder/museum.rb
@@ -2,10 +2,28 @@ class MuseumFinder::Museum
 
   attr_accessor :name, :location, :hours, :admission, :overview, :highlights, :transportation
 
-    def self.create_new_museum
+  @@all = []
+
+    def initialize(name = nil, url = nil)
+      @name = name
+      @url = url
+    end
+
+    def self.all
+      @@all
+    end
+
+    def self.new_museum
+
       # placeholder - may change
       # put code here
+      museums = MuseumFinder::Scraper.scrape_landing_page
+      museums.css("h3.title").each do |museum|
+        museum.text
+
+      binding.pry
     end
+  end
 
     def self.museum_info
 
--- museum_finder/lib/museum_finder/scraper.rb
+++ museum_finder/lib/museum_finder/scraper.rb
@@ -2,13 +2,11 @@ class MuseumFinder::Scraper
 
   def self.scrape_landing_page
     museums_landing = Nokogiri::HTML(open("https://www.si.edu/museums"))
-    #museums = []
-
-    museums_landing.css("h3.title").each_with_index do |museum, index|
-      museum_name = museum.text
-      number = index + 1
-    end
-    end
+      museums_landing.css("h3.title")
+  #    museum_name = museum.text
+  #    number = index + 1
+  #  end
+  #  end
   end
 
   def self.scrape_url
@@ -26,15 +24,15 @@ class MuseumFinder::Scraper
   def scrape_museum_page
     # placeholder
     museum_page = Nokogiri::HTML(open("https://www.si.edu/museums/american-history-museum"))
-    museum_page.css("div.edan-record--location").each do |museum|
-      address = museum.css("div.location-address").children[0].text.delete("\n").strip
-      city = museum.css("div.location-address").children[2].text.strip
-      hours = museum.css("div.location-hours").text.gsub(/\r\n/," - ").strip
-      admission = museum.css("div.location-admission").text.delete("\n").strip
-      about = museum.css("div.info p").children[0].text
-      highlights = museum.css("div.info p").children[1].text
-      binding.pry
-    end
+      museum_page.css("div.edan-record--location").each do |museum|
+        name = museum.css("h1.page-title")
+        address = museum.css("div.location-address").children[0].text.delete("\n").strip
+        city = museum.css("div.location-address").children[2].text.strip
+        hours = museum.css("div.location-hours").text.gsub(/\r\n/," - ").strip
+        admission = museum.css("div.location-admission").text.delete("\n").strip
+        about = museum.css("div.info p").children[0].text
+        highlights = museum.css("div.info p").children[1].text
+      end
   end
 
   end
