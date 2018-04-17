d37f001ddf468f981f808bf1112ba1ebc5701fe5
Michelle Olivieri <michelleo72@gmail.com>
2018-04-17 16:50:34 +0000

    scraper edits

--- museum_finder/lib/museum_finder/scraper.rb
+++ museum_finder/lib/museum_finder/scraper.rb
@@ -6,7 +6,9 @@ class MuseumFinder::Scraper
 
     link = museums_landing.css("h3.title").children.css("a").map { |x| x.attribute('href').value}
       link.each do |y|
-         url = "https://si.edu/#{y}"
+      @url = "https://si.edu/#{y}"
+
+
 
     museums_landing.css("h3.title").each_with_index do |museum, index|
       museum_name = museum.text
@@ -15,8 +17,27 @@ class MuseumFinder::Scraper
     end
   end
 
-  def self.scrape_museum_info_page
+  def self.scrape_test
+    museums_landing = Nokogiri::HTML(open("https://www.si.edu/museums"))
+    #museums = []
 
+    link = museums_landing.css("h3.title").children.css("a").map { |x| x.attribute('href').value}
+      link.each do |y|
+      url = "https://si.edu/#{y}"
+
+    end
+    end
+
+  def self.scrape_museum_info_page
+    # placeholder for testing
+    museum_page = Nokogiri::HTML(open("https://www.si.edu/museums/american-history-museum"))
+    museum_page.css("div.location-info").each do |museum|
+      address = museum.css("div.location-address").children[0].text.delete("\n").strip
+      city = museum.css("div.location-address").children[2].text.strip
+
+    binding.pry
+
+  end
   end
 
 
