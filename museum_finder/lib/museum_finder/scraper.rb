1d036c9705b66a924ceda6cf7fb14f942717b519
Michelle Olivieri <michelleo72@gmail.com>
2018-04-17 20:10:23 +0000

    scraper edits

--- museum_finder/lib/museum_finder/scraper.rb
+++ museum_finder/lib/museum_finder/scraper.rb
@@ -1,14 +1,8 @@
 class MuseumFinder::Scraper
 
-  def self.scrape_museums_landing_page
+  def self.scrape_landing_page
     museums_landing = Nokogiri::HTML(open("https://www.si.edu/museums"))
     #museums = []
-
-    link = museums_landing.css("h3.title").children.css("a").map { |x| x.attribute('href').value}
-      link.each do |y|
-      @url = "https://si.edu/#{y}"
-
-
 
     museums_landing.css("h3.title").each_with_index do |museum, index|
       museum_name = museum.text
@@ -17,9 +11,9 @@ class MuseumFinder::Scraper
     end
   end
 
-  def self.scrape_test
+  def self.scrape_url
     museums_landing = Nokogiri::HTML(open("https://www.si.edu/museums"))
-    #museums = []
+
 
     link = museums_landing.css("h3.title").children.css("a").map { |x| x.attribute('href').value}
       link.each do |y|
