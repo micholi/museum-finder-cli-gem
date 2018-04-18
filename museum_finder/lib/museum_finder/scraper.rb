2195455d492471f0a8ab57f9c45e132c9ba523f7
Michelle Olivieri <michelleo72@gmail.com>
2018-04-18 14:46:59 +0000

    removed no longer needed scraper method

--- museum_finder/lib/museum_finder/scraper.rb
+++ museum_finder/lib/museum_finder/scraper.rb
@@ -8,20 +8,4 @@ class MuseumFinder::Scraper
     self.get_page.css("div.content")
   end
 
-  def self.scrape_museum_page
-    # placeholder
-    museum_page = Nokogiri::HTML(open("https://www.si.edu/museums/american-history-museum"))
-      museum_page.css("div.edan-record--location").each do |museum|
-        name = museum.css("h1.page-title")
-        address = museum.css("div.location-address").children[0].text.delete("\n").strip
-        city = museum.css("div.location-address").children[2].text.strip
-        hours = museum.css("div.location-hours").text.gsub(/\r\n/," - ").strip
-        admission = museum.css("div.location-admission").text.delete("\n").strip
-        about = museum.css("div.info p").children[0].text
-        highlights = museum.css("div.info p").children[1].text
-        transportation = museum.css("div.panel p").text
-        binding.pry
-      end
-  end
-
-  end
+end
