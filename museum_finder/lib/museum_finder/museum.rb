0db08e623ddb2635ab5fa7858675ec1c4643f9a3
Michelle Olivieri <michelleo72@gmail.com>
2018-04-26 00:38:39 +0000

    new method to call museum scraper

--- museum_finder/lib/museum_finder/museum.rb
+++ museum_finder/lib/museum_finder/museum.rb
@@ -7,57 +7,29 @@ class MuseumFinder::Museum
     def initialize(museum_hash)
       museum_hash.each do |attribute, value|
         self.send("#{attribute}=", value)
-
-
-
-end
-
+      end
+      
       @@all << self
-
     end
 
     def self.create_museums
       MuseumFinder::Scraper.scrape_landing_page.each do |museum_hash|
         self.new(museum_hash)
-
       end
     end
 
-
-
-
-
-def add_museum_attributes
-
-
-    url = self.url
-    MuseumFinder::Scraper.scrape_museum_page(url).each do |key, value|
-      #binding.pry
-
-      self.send("#{key}=", value)
-
-
-  #  end
-  end
-end
-
-
-    def self.all
-      #create_museums
-      @@all
-
+    def add_museum_attributes
+      MuseumFinder::Scraper.scrape_museum_page(self.url).each do |key, value|
+        self.send("#{key}=", value)
+      end
     end
 
-
-
-
-
-
+    def self.all
+      @@all
+    end
 
     def self.find(number)
       self.all[number-1]
     end
-
-
 
 end
